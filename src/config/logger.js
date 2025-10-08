016fd2ed11c23ad23217e87304dc404c513c2d41
Elad Gelerenter <eladg3@gmail.com>
2025-10-08 18:49:02 +0300

    Prettier fix

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,12 +5,14 @@ const isProd = process.env.NODE_ENV === 'production';
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
   base: { service: 'cozycup-api', env: process.env.NODE_ENV },
-  ...(isProd ? {} : {
-    transport: {
-      target: 'pino-pretty',
-      options: { singleLine: true }
-    }
-  })
+  ...(isProd
+    ? {}
+    : {
+        transport: {
+          target: 'pino-pretty',
+          options: { singleLine: true },
+        },
+      }),
 });
 
 export default logger;
