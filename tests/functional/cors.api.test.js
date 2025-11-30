1966cc61a51e169ebbf9bb005a379c6f17514843
Elad Gelerenter <eladg3@gmail.com>
2025-11-30 22:27:56 +0200

    Fix cors test

--- tests/functional/cors.api.test.js
+++ tests/functional/cors.api.test.js
@@ -5,10 +5,10 @@ describe('CORS', () => {
   test('OPTIONS request should respond with valid CORS preflight', async () => {
     const res = await request(app)
       .options('/health')
-      .set('Origin', 'http://example.com')
+      .set('Origin', 'http://127.0.0.1:5173')
       .set('Access-Control-Request-Method', 'GET');
 
-    expect([200, 204, 500]).toContain(res.status);
+    expect([200, 204]).toContain(res.status);
 
     expect(res.headers).toHaveProperty('access-control-allow-methods');
     expect(res.headers['access-control-allow-methods']).toMatch(/GET|POST|OPTIONS/);
